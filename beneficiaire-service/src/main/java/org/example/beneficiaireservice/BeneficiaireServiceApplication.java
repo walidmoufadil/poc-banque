0ef02d7f19fc662380734a4fd91235e99b2cbbe1
MOUFADIL walid <walid.moufadil-etu@etu.univh2c.ma>
2025-11-12 21:00:46 +0100

    add beneficiare service

---
+++ beneficiaire-service/src/main/java/org/example/beneficiaireservice/BeneficiaireServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.beneficiaireservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeneficiaireServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeneficiaireServiceApplication.class, args);
+    }
+
+}
